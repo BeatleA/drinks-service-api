998436c86ba57600651be40811119e42be57ebf2
Anna W <tester.kodilla@gmail.com>
2021-11-17 13:55:28 +0000

    GET request for coffee endpoint and test

---
+++ src/controllers/coffee_request.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Test coffeelover API endpoint request', () => {
+    test('GET should return correct message', async () => {
+        const res = await request(app).get('/coffeelover');
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toEqual('I like coffee!');
+    });
+});
+
+describe('Test coffee API endpoint request', () => {
+    test('GET /coffee should return correct object', async () => {
+        const res = await request(app)
+            .get('/coffee')
+            .query({ coffeeName: 'Latte' });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual({
+            drinkType: 'Coffee',
+            name: 'Latte',
+        });
+    });
+});
